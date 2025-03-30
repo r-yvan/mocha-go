56f45c69b01654ae0f96ec9c52d2eb6473f14597
r-yvan <yvankiliye.rubuto@gamil.com>
2025-03-30 17:47:47 +0200

    Commit

--- app/index.tsx
+++ app/index.tsx
@@ -14,12 +14,18 @@ const Onboarding = () => {
         colors={["#00000000", "#000", "#000", "#000"]}
         className="absolute bottom-0 w-full h-1/2"
       />
-      <View className="w-full h-1/2 justify-center items-center bg-gradient-to-t from-black to-transparent">
+      <View className="w-full h-1/2 justify-center px-10 items-center bg-gradient-to-t from-black to-transparent">
         <Text className="text text-white text-4xl text-center font-sbold">
           Fall in love with Coffee in a blissful delight
         </Text>
+        <Text className="text-white/80 font-slight text-center ">
+          Welcome to our cozy coffee corner, where every cup is delightful for
+          you!!
+        </Text>
         <TouchableOpacity>
-          
+          <View className="bg-[#C67C48] px-20 py-4 rounded-3xl">
+            <Text className="text-white tex-lg font-sbold">Get Started</Text>
+          </View>
         </TouchableOpacity>
       </View>
     </View>
